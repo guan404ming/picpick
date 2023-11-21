142371a1ef744a65c794ae7b978be8026948663c
jill-ntu <b09610011@ntu.edu.tw>
2023-11-21 23:37:18 +0800

    style: update

--- src/app/saves/page.tsx
+++ src/app/saves/page.tsx
@@ -5,7 +5,7 @@ export default function SavesPage() {
   <div className="card">
       <img src="image1.jpg" alt="Book Cover" />
       <div className="card-content">
-          <h2>繪本名稱 1</h2>
+          <h2>繪本名稱</h2>
           <input type="range" value="50" />
       </div>
   </div>
@@ -13,7 +13,7 @@ export default function SavesPage() {
   <div className="card">
       <img src="image2.jpg" alt="Book Cover" />
       <div className="card-content">
-          <h2>繪本名稱 2</h2>
+          <h2>繪本名稱</h2>
           <input type="range" value="50" />
       </div>
   </div>
