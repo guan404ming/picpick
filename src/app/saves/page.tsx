9a30e8f7936a67c64c8f865f88972294a8e43246
jill-ntu <b09610011@ntu.edu.tw>
2023-11-24 20:44:27 +0800

    11/24 update

--- src/app/saves/page.tsx
+++ src/app/saves/page.tsx
@@ -1,6 +1,28 @@
 import React from 'react';
+
+interface Book {
+  id: number;
+  title: string;
+  image: string;
+}
+
+interface CardProps {
+  title: string;
+  image: string;
+}
+
+function Card({ title, image }: CardProps) {
+  return (
+    <div style={{ width: '200px', margin: '10px' }}>
+      <h3>{title}</h3>
+      <img src={image} alt={title} style={{ maxWidth: '100%' }} />
+      <input type="range" />
+    </div>
+  );
+}
+
 export default function SavesPage() {
-  const books = [
+  const books: Book[] = [
     { id: 1, title: 'Book 1', image: 'book1.jpg' },
     { id: 2, title: 'Book 2', image: 'book2.jpg' },
     { id: 3, title: 'Book 3', image: 'book3.jpg' },
@@ -20,13 +42,3 @@ export default function SavesPage() {
     </div>
   );
 }
-
-function Card({ title, image }) {
-  return (
-    <div style={{ width: '200px', margin: '10px' }}>
-      <h3>{title}</h3>
-      <img src={image} alt={title} style={{ maxWidth: '100%' }} />
-      <input type="range" />
-    </div>
-  );
-} 
