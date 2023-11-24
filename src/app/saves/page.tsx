6a8fe382804432dfd3bbf782717e69f208e51f45
jill-ntu <b09610011@ntu.edu.tw>
2023-11-24 19:15:12 +0800

    saves style: update

--- src/app/saves/page.tsx
+++ src/app/saves/page.tsx
@@ -1,23 +1,36 @@
+import React from 'react';
 export default function SavesPage() {
-  return <div>
-  <h1>Saves</h1>
-
-  <div className="card">
-      <img src="image1.jpg" alt="Book Cover" />
-      <div className="card-content">
-          <h2>繪本名稱</h2>
-          <input type="range" value="50" />
-      </div>
-  </div>
-
-  <div className="card">
-      <img src="image2.jpg" alt="Book Cover" />
-      <div className="card-content">
-          <h2>繪本名稱</h2>
-          <input type="range" value="50" />
-      </div>
-  </div>
-
-  </div>;
+    return (
+        <div className="card w-[350px]">
+          <div className="card-header">
+            <h2 className="card-title">Create project</h2>
+            <p className="card-description">Deploy your new project in one-click.</p>
+          </div>
+          <div className="card-content">
+            <form>
+              <div className="grid w-full items-center gap-4">
+                <div className="flex flex-col space-y-1.5">
+                  <label htmlFor="name">Name</label>
+                  <input id="name" type="text" placeholder="Name of your project" />
+                </div>
+                <div className="flex flex-col space-y-1.5">
+                  <label htmlFor="framework">Framework</label>
+                  <select id="framework">
+                    <option value="" disabled selected>Select</option>
+                    <option value="next">Next.js</option>
+                    <option value="sveltekit">SvelteKit</option>
+                    <option value="astro">Astro</option>
+                    <option value="nuxt">Nuxt.js</option>
+                  </select>
+                </div>
+              </div>
+            </form>
+          </div>
+          <div className="card-footer flex justify-between">
+            <button className="btn btn-outline">Cancel</button>
+            <button className="btn btn-primary">Deploy</button>
+          </div>
+        </div>
+      );
   
 } 
