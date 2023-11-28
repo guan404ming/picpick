7c9da80c9ff7f348ccaac094e5efc5197f8912fd
GUAN-MING <guanmingchiu@gmail.com>
2023-11-29 01:04:40 +0800

    fix: homepage layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -28,28 +28,48 @@ export default async function Home() {
 
   return (
     <div className="w-full p-6">
-      <div className=" grid grid-cols-1 gap-7 rounded-lg bg-[#F0F0F0] p-10 text-center drop-shadow dark:bg-slate-800">
-        <p className="truncate text-5xl font-bold">Picbook Home</p>
-        <p className="truncate">
-          SloganSloganSloganSloganSloganSloganSloganSloganSlogan
-        </p>
-        <Button className="mx-auto w-1/4 p-6 text-xl drop-shadow-lg">
+      <div className="grid grid-cols-1 gap-7 rounded-lg bg-[#F0F0F0] p-10 text-center drop-shadow dark:bg-slate-800">
+        <p className="truncate text-4xl font-bold">PICPICK</p>
+
+        <div className="text-xl">
+          <p className="truncate font-bold">Make a click, we take a pick.</p>
+          <p className="truncate">
+            Unlock your Personalized PicBook with PICPICK!
+          </p>
+        </div>
+
+        <Button className="mx-auto w-1/6 p-6 text-xl drop-shadow-lg">
           Start
         </Button>
       </div>
 
       <Separator className="my-8"></Separator>
 
-      <div className="flex items-center px-[10%] py-10">
-        <p className="text-3xl font-bold">Picbook Home</p>
-        <div className="ml-[20%] grid grid-cols-1 gap-4">
-          <p className="truncate text-xl">Picbook Home Picbook</p>
-          <p className="truncate text-xl">Picbook Home</p>
-          <p className="truncate text-xl">Picbook </p>
-          <p className="truncate text-xl">Picbook Home Picbook Home</p>
-          <p className="truncate text-xl">
-            Picbook Home Picbook Home Picbook Home
-          </p>
+      <div className="grid grid-cols-6 py-10">
+        <p className="col-span-2 flex items-center justify-center text-center text-3xl font-bold">
+          About
+        </p>
+        <div className="col-span-4 pr-20 text-xl leading-normal">
+          PICPICK is a picture book recommendation website where users can
+          receive AI-curated book suggestions by responding to psychological
+          assessment questions. Users can explore, read, and collect the
+          recommended picture books. Through the design, we aim to create a
+          therapeutic and inspiring experience for readers, providing solace and
+          inspiration in their daily lives.
+        </div>
+      </div>
+
+      <div className="grid grid-cols-6 py-10">
+        <p className="col-span-2 flex items-center justify-center text-center text-3xl font-bold">
+          Introduction
+        </p>
+        <div className="col-span-4 pr-20 text-xl leading-normal">
+          PICPICK is a picture book recommendation website where users can
+          receive AI-curated book suggestions by responding to psychological
+          assessment questions. Users can explore, read, and collect the
+          recommended picture books. Through the design, we aim to create a
+          therapeutic and inspiring experience for readers, providing solace and
+          inspiration in their daily lives.
         </div>
       </div>
     </div>
