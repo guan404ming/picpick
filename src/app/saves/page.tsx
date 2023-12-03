6ca2d1fb4e4bef328b229904d8563efe154fd45a
jill-ntu <150576738+jill-ntu@users.noreply.github.com>
2023-12-03 12:32:42 +0800

    feat: add saves page

--- src/app/saves/page.tsx
+++ src/app/saves/page.tsx
@@ -1,3 +1,31 @@
+import Book from "./_components/Book";
+
 export default function SavesPage() {
-  return <div></div>;
+  const books = [
+    { id: 1, title: "Book 1", image: "book1.jpg" },
+    { id: 2, title: "Book 2", image: "book1.jpg" },
+    { id: 3, title: "Book 3", image: "book1.jpg" },
+    { id: 4, title: "Book 4", image: "book1.jpg" },
+    { id: 5, title: "Book 5", image: "book1.jpg" },
+    { id: 6, title: "Book 6", image: "book1.jpg" },
+    { id: 1, title: "Book 1", image: "book1.jpg" },
+    { id: 2, title: "Book 2", image: "book1.jpg" },
+    { id: 3, title: "Book 3", image: "book1.jpg" },
+    { id: 4, title: "Book 4", image: "book1.jpg" },
+    { id: 5, title: "Book 5", image: "book1.jpg" },
+    { id: 6, title: "Book 6", image: "book1.jpg" },
+  ];
+
+  return (
+    <div className="block max-h-screen w-full overflow-scroll p-8">
+      <h1 className="mb-10 text-3xl font-bold">Saves</h1>
+      <div className="flex w-full flex-col items-center justify-center">
+        <div className="grid gap-x-20 gap-y-14 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 ">
+          {books.map((book, idx) => (
+            <Book key={idx} book={book}></Book>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 }
