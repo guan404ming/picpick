9d6faae7c09e6ff6280bbad088cd68bad35e3578
stellaaacheng <81860742+stellaaacheng@users.noreply.github.com>
2023-12-03 20:43:48 +0800

    feat: add favorite book api

--- src/app/api/favourites/[userId]/route.tsx
+++ src/app/api/favourites/[userId]/route.tsx
@@ -1,9 +1,27 @@
 import { NextApiRequest, NextApiResponse } from 'next';
-import { serial, varchar, timestamp } from 'drizzle-orm/pg-core';
 import { db } from "@/db";
 import { eq } from "drizzle-orm";
-import { bookTable } from "@/db/schema";
+import { favourites } from '@/db/schema';
 
 export default async function GET(req: NextApiRequest, res: NextApiResponse) {
+    const userId = parseInt(req.query.userId as string, 10);
+  
+    if(!userId) {
+      return res.status(404).json( { error: 'book id cannot be null' } );
+    }
+
+    try {
+        const book = await db.select().from(favourites).where(eq(favourites.userId, userId));
+        
+        if(book.length === 0)
+          return res.status(404).json(
+            { error: 'cannot find book id "${userId}"'},
+          )
+        return res.json(book[0]);
     
+      } catch (error) {
+        console.log(error);
+        return res.status(500).json( { error: "Something went wrong" } );
+      }
 }
+
