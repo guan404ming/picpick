187e367566532a0c98a4da31cf12528e796ae4f9
stellaaacheng <81860742+stellaaacheng@users.noreply.github.com>
2023-12-03 22:22:37 +0800

    feat: add message api

--- src/app/api/message/[userId]/route.tsx
+++ src/app/api/message/[userId]/route.tsx
@@ -1,7 +1,7 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import { db } from "@/db";
 import { eq } from "drizzle-orm";
-import { favourites } from '@/db/schema';
+import { messageTable } from '@/db/schema';
 
 export default async function GET(req: NextApiRequest, res: NextApiResponse) {
     const userId = parseInt(req.query.userId as string, 10);
@@ -11,13 +11,13 @@ export default async function GET(req: NextApiRequest, res: NextApiResponse) {
     }
 
     try {
-        const book = await db.select().from(favourites).where(eq(favourites.userId, userId));
+        const messages = await db.select().from(messageTable).where(eq(messageTable.userId, userId));
         
-        if(book.length === 0)
+        if(messages.length === 0)
           return res.status(404).json(
             { error: 'cannot find user id "${userId}"'},
           )
-        return res.json(book[0]);
+        return res.json(messages);
     
       } catch (error) {
         console.log(error);
