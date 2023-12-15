22b2888a5ac39d4c9c6e50b475c3ee4613a72b5e
GUAN-MING <guanmingchiu@gmail.com>
2023-12-16 01:55:50 +0800

    fix: remove introduction section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -36,20 +36,6 @@ export default async function Home() {
           inspiration in their daily lives.
         </div>
       </div>
-
-      <div className="grid grid-cols-6 py-10 max-md:flex max-md:flex-col">
-        <p className="col-span-2 flex items-center justify-center text-center text-3xl font-bold">
-          Introduction
-        </p>
-        <div className="max-md:text-md col-span-4 pr-20 text-xl leading-normal max-md:px-2 max-md:pt-6 max-md:text-sm">
-          PICPICK is a picture book recommendation website where users can
-          receive AI-curated book suggestions by responding to psychological
-          assessment questions. Users can explore, read, and collect the
-          recommended picture books. Through the design, we aim to create a
-          therapeutic and inspiring experience for readers, providing solace and
-          inspiration in their daily lives.
-        </div>
-      </div>
     </div>
   );
 }
