83176c27cb10838c16948bbf4f6e3dc34c886825
GUAN-MING <guanmingchiu@gmail.com>
2023-12-20 12:19:31 +0800

    fix: add remain loading

--- src/app/chat/_components/GreetingMessage.tsx
+++ src/app/chat/_components/GreetingMessage.tsx
@@ -5,33 +5,38 @@ import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
 import useChat from "@/hooks/useChat";
 
+import LoadingMessage from "./LoadingMessage";
+
 export default function GreetingMessage() {
-  const { handleGetQuestion } = useChat();
+  const { handleGetQuestion, loading } = useChat();
 
   return (
-    <div className={"items-top flex flex-row space-x-1.5"}>
-      <Avatar className="mr-2 mt-2 h-6 w-6 [.other:has(+.other)>&]:opacity-0">
-        <AvatarImage src={picPick.src} alt="pic-pick" />
-        <AvatarFallback />
-      </Avatar>
+    <>
+      <div className={"items-top flex flex-row space-x-1.5"}>
+        <Avatar className="mr-2 mt-2 h-6 w-6 [.other:has(+.other)>&]:opacity-0">
+          <AvatarImage src={picPick.src} alt="pic-pick" />
+          <AvatarFallback />
+        </Avatar>
 
-      <div
-        className={
-          "my-2 flex flex-col justify-center space-y-2 rounded-xl bg-[#88888840] p-4 text-black dark:text-white"
-        }
-      >
-        <p className="text-sm">Hello, let's start to chat! </p>
-        <Button
-          className="block"
-          size="lg"
-          onClick={async (e) => {
-            e.preventDefault();
-            await handleGetQuestion();
-          }}
+        <div
+          className={
+            "my-2 flex flex-col justify-center space-y-2 rounded-xl bg-[#88888840] p-4 text-black dark:text-white"
+          }
         >
-          Start
-        </Button>
+          <p className="text-sm">Hello, let's start to chat! </p>
+          <Button
+            className="block"
+            size="lg"
+            onClick={async (e) => {
+              e.preventDefault();
+              await handleGetQuestion();
+            }}
+          >
+            Start
+          </Button>
+        </div>
       </div>
-    </div>
+      {loading && <LoadingMessage></LoadingMessage>}
+    </>
   );
 }
--- src/app/chat/_components/Message.tsx
+++ src/app/chat/_components/Message.tsx
@@ -9,6 +9,8 @@ import useChat from "@/hooks/useChat";
 import type { SelectBook, SelectMessage, SelectQuestion } from "@/lib/types/db";
 import { cn } from "@/lib/utils";
 
+import LoadingMessage from "./LoadingMessage";
+
 type MessageProps = {
   message: {
     MESSAGE: SelectMessage;
@@ -27,7 +29,7 @@ export default function Message({
   count,
   setAnswerList,
 }: MessageProps) {
-  const { handleGetQuestion, handlePostAnswer } = useChat();
+  const { handleGetQuestion, handlePostAnswer, loading } = useChat();
 
   const isSystemMessage = message.MESSAGE.sender === "system";
   const containerClasses = cn(
@@ -37,61 +39,64 @@ export default function Message({
   );
 
   return (
-    <div className={containerClasses}>
-      {isSystemMessage && (
-        <Avatar className="mr-2 mt-2 h-6 w-6 [.other:has(+.other)>&]:opacity-0">
-          <AvatarImage src={picPick.src} alt="pic-pick" />
-          <AvatarFallback />
-        </Avatar>
-      )}
+    <>
+      <div className={containerClasses}>
+        {isSystemMessage && (
+          <Avatar className="mr-2 mt-2 h-6 w-6 [.other:has(+.other)>&]:opacity-0">
+            <AvatarImage src={picPick.src} alt="pic-pick" />
+            <AvatarFallback />
+          </Avatar>
+        )}
 
-      <div
-        className={cn(
-          "my-2 flex flex-col justify-center space-y-2 rounded-xl p-4",
-          message.MESSAGE.sender === "user"
-            ? "bg-black text-white"
-            : "bg-[#88888840] text-black dark:text-white",
-        )}
-      >
-        <p className="text-sm">{message.MESSAGE.content}</p>
-        {message.QUESTION && (
-          <>
-            <div className="max-w-1/4 flex flex-col text-sm">
-              {message.options?.map(
-                (option, idx) =>
-                  option && (
-                    <p key={idx}>
-                      {idx + 1}. {option}
-                    </p>
-                  ),
-              )}
-            </div>
-            <div className="grid grid-cols-1 gap-2 sm:grid-cols-2">
-              {message.options?.map(
-                (option, idx) =>
-                  option && (
-                    <Button
-                      className="block"
-                      key={idx}
-                      onClick={async (e) => {
-                        e.preventDefault();
-                        await handlePostAnswer({ answer: `${idx + 1}` });
-                        setCount((prev) => prev + 1);
-                        setAnswerList((prev) => [
-                          ...prev,
-                          message.options[idx],
-                        ]);
-                        if (count + 1 < 3) await handleGetQuestion();
-                      }}
-                    >
-                      {idx + 1}
-                    </Button>
-                  ),
-              )}
-            </div>
-          </>
-        )}
+        <div
+          className={cn(
+            "my-2 flex flex-col justify-center space-y-2 rounded-xl p-4",
+            message.MESSAGE.sender === "user"
+              ? "bg-black text-white"
+              : "bg-[#88888840] text-black dark:text-white",
+          )}
+        >
+          <p className="text-sm">{message.MESSAGE.content}</p>
+          {message.QUESTION && (
+            <>
+              <div className="max-w-1/4 flex flex-col text-sm">
+                {message.options?.map(
+                  (option, idx) =>
+                    option && (
+                      <p key={idx}>
+                        {idx + 1}. {option}
+                      </p>
+                    ),
+                )}
+              </div>
+              <div className="grid grid-cols-1 gap-2 sm:grid-cols-2">
+                {message.options?.map(
+                  (option, idx) =>
+                    option && (
+                      <Button
+                        className="block"
+                        key={idx}
+                        onClick={async (e) => {
+                          e.preventDefault();
+                          await handlePostAnswer({ answer: `${idx + 1}` });
+                          setCount((prev) => prev + 1);
+                          setAnswerList((prev) => [
+                            ...prev,
+                            message.options[idx],
+                          ]);
+                          if (count + 1 < 3) await handleGetQuestion();
+                        }}
+                      >
+                        {idx + 1}
+                      </Button>
+                    ),
+                )}
+              </div>
+            </>
+          )}
+        </div>
       </div>
-    </div>
+      {loading && <LoadingMessage></LoadingMessage>}
+    </>
   );
 }
